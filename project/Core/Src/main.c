bfebeed4ef497f2e9c796e5d516cebcb5940e163
Dunets Andrii <andrijdunec@gmail.com>
2022-08-23 19:33:11 +0300

    producer-consumer baro

--- project/Core/Src/main.c
+++ project/Core/Src/main.c
@@ -18,24 +18,31 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "cmsis_os.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
+
 #include "baro.h"
+#include "lcd.h"
+#include <stdio.h>	// snprintf
+#include <string.h>	// memset
+#include <math.h>	// sin
+#include <stdlib.h>	// rand
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
+typedef StaticSemaphore_t osStaticSemaphoreDef_t;
 /* USER CODE BEGIN PTD */
 
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define countof(_a) (sizeof(_a)/sizeof(_a[0]))
+#define countof(_a)	(sizeof(_a) / sizeof(_a[0]))
+#define UART_RX_BUF_SIZE	200
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -46,50 +53,94 @@
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
 
-UART_HandleTypeDef huart1;
+SPI_HandleTypeDef hspi1;
 
+UART_HandleTypeDef huart1;
+DMA_HandleTypeDef hdma_usart1_rx;
+
+/* Definitions for taskLEDBlink */
+osThreadId_t taskLEDBlinkHandle;
+const osThreadAttr_t taskLEDBlink_attributes = {
+  .name = "taskLEDBlink",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for taskButtonRead */
+osThreadId_t taskButtonReadHandle;
+const osThreadAttr_t taskButtonRead_attributes = {
+  .name = "taskButtonRead",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for muxUART */
+osMutexId_t muxUARTHandle;
+const osMutexAttr_t muxUART_attributes = {
+  .name = "muxUART"
+};
+/* Definitions for muxLCD */
+osMutexId_t muxLCDHandle;
+const osMutexAttr_t muxLCD_attributes = {
+  .name = "muxLCD"
+};
+/* Definitions for sem1 */
+osSemaphoreId_t sem1Handle;
+osStaticSemaphoreDef_t sem1ControlBlock;
+const osSemaphoreAttr_t sem1_attributes = {
+  .name = "sem1",
+  .cb_mem = &sem1ControlBlock,
+  .cb_size = sizeof(sem1ControlBlock),
+};
+/* Definitions for sem2 */
+osSemaphoreId_t sem2Handle;
+osStaticSemaphoreDef_t sem2ControlBlock;
+const osSemaphoreAttr_t sem2_attributes = {
+  .name = "sem2",
+  .cb_mem = &sem2ControlBlock,
+  .cb_size = sizeof(sem2ControlBlock),
+};
 /* USER CODE BEGIN PV */
+static uint8_t rx_buffer[UART_RX_BUF_SIZE];
+static uint8_t *rx_head = rx_buffer, *rx_tail = rx_buffer;
 
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_I2C1_Init(void);
+static void MX_SPI1_Init(void);
+void StartTaskLEDBlink(void *argument);
+void StartTaskButtonRead(void *argument);
+
 /* USER CODE BEGIN PFP */
-
-#define maxSamples 200
-static int32_t filter(int32_t sample)
-{
-	static int32_t arr[maxSamples] = {0};
-	static uint32_t runner = 0;
-	static uint8_t filled = 0;
-
-	arr[runner] = sample;
-
-	if (filled < maxSamples)
-		++filled;
-
-	if (++runner == maxSamples)
-		runner = 0;
-
-	int64_t avg = 0;
-
-	for (uint32_t i = 0; i < filled; i++)
-	{
-		avg += arr[i];
-	}
-
-	avg /= filled;
-
-	return (int32_t)avg;
-}
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+char text[100];
+
+int __io_getchar(void) {
+	rx_head = &rx_buffer[UART_RX_BUF_SIZE - hdma_usart1_rx.Instance->NDTR];
+
+	while (rx_tail == rx_head) {
+		rx_head = &rx_buffer[UART_RX_BUF_SIZE - hdma_usart1_rx.Instance->NDTR];
+	}
+
+	uint8_t b = *rx_tail;
+
+	if (++rx_tail == (rx_buffer + UART_RX_BUF_SIZE))
+		rx_tail = rx_buffer;
+
+	return (int)b;
+}
+
+int __io_putchar(int ch) {
+	HAL_UART_Transmit(&huart1, (uint8_t*)&ch, 1, 100);
+	return 0;
+}
 
 /* USER CODE END 0 */
 
@@ -100,7 +151,7 @@ static int32_t filter(int32_t sample)
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	uint32_t cnt = 0;
+
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -121,43 +172,111 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_USART1_UART_Init();
   MX_I2C1_Init();
+  MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
-  char text[100] = {0};
+
+  if (HAL_UART_Receive_DMA(&huart1, rx_buffer, sizeof(rx_buffer)) != HAL_OK)
+  {
+	  snprintf(text, countof(text), "Error start UART RX %d\n", __LINE__);
+	  HAL_UART_Transmit(&huart1, (uint8_t*)text, strnlen(text, countof(text)), 1000);
+	  while (1) {}
+  }
+
+  //snprintf(text, countof(text), "Start UART RX %d\n", __LINE__);
+  //HAL_UART_Transmit(&huart1, (uint8_t*)text, strnlen(text, countof(text)), 1000);
+
   if (baro_init() != BARO_OK)
   {
 	  snprintf(text, countof(text), "Error init baro\n");
 	  HAL_UART_Transmit(&huart1, (uint8_t*)text, strnlen(text, countof(text)), 1000);
-	  while(1){};
+	  while (1) {}
   }
+
+//  for (uint16_t p = 0; p < 100; p++) {
+//	  lcd_pixel(p, p+0, ST7735_RED);
+//	  lcd_pixel(p, p+1, ST7735_GREEN);
+//	  lcd_pixel(p, p+2, ST7735_BLUE);
+//  }
+//
+//  lcd_fill_rect(10, 10, 50, 50, ST7735_CYAN);
+//  lcd_fill_rect(50, 50, 150, 150, ST7735_MAGENTA);
+//  lcd_rect(5, 5, 15, 15, ST77XX_ORANGE);
+//  lcd_rect(200, 200, 15, 15, ST77XX_ORANGE);
+//  lcd_line(13, 19, 37, 93, ST77XX_GREEN);
+//  lcd_line(13, 19, 93, 37, ST77XX_GREEN);
+//  lcd_circle(22, 55, 76, ST77XX_RED);
+//  lcd_fill_circle(33, 66, 19, ST77XX_BLUE);
+//  lcd_print("Hello LCD!");
+//  lcd_set_text_color(ST7735_CYAN);
+//  lcd_set_text_bg_color(ST7735_ORANGE);
+//  lcd_print("\nNew line!");
 
   /* USER CODE END 2 */
 
+  /* Init scheduler */
+  osKernelInitialize();
+  /* Create the mutex(es) */
+  /* creation of muxUART */
+  muxUARTHandle = osMutexNew(&muxUART_attributes);
+
+  /* creation of muxLCD */
+  muxLCDHandle = osMutexNew(&muxLCD_attributes);
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* Create the semaphores(s) */
+  /* creation of sem1 */
+  sem1Handle = osSemaphoreNew(1, 1, &sem1_attributes);
+
+  /* creation of sem2 */
+  sem2Handle = osSemaphoreNew(1, 1, &sem2_attributes);
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of taskLEDBlink */
+  taskLEDBlinkHandle = osThreadNew(StartTaskLEDBlink, NULL, &taskLEDBlink_attributes);
+
+  /* creation of taskButtonRead */
+  taskButtonReadHandle = osThreadNew(StartTaskButtonRead, NULL, &taskButtonRead_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  //lcd_init();
+  //baro_init();
+
+  sem1Handle = osSemaphoreNew(1, 1, &sem1_attributes);//producer should starts first
+  sem2Handle = osSemaphoreNew(1, 0, &sem2_attributes);
+
+  /* USER CODE END RTOS_EVENTS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-
-  /*int32_t signal = {0};
-  int32_t signalNoised = {0};
-  int32_t signalFiltered  = {0};*/
-
   while (1)
   {
-	  /*signal = sin((3.14 * cnt) / 1000.0) * 50 + 50;
-	  signalNoised = signal+(rand()%20)-10;
-	  signalFiltered = filter(signalNoised);*/
-
-	  //sprintf(text, "/*%ld,%ld,%ld*/\n", signal, signalNoised, signalFiltered);
-	  /*cnt++;
-	  HAL_UART_Transmit(&huart1, (uint8_t*)text, strlen(text), 1000);
-
-	  HAL_Delay(1);*/
-
-	  int32_t temp = baro_read_temp();
-	  int32_t pres = baro_read_press();
-	  snprintf(text, countof(text), "/*%ld.%02ld,%ld.%02ld*/\n", temp/100, temp%100, pres/100, pres%100);
-	  HAL_UART_Transmit(&huart1, (uint8_t*)text, strlen(text), 1000);
-
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -245,6 +364,44 @@ static void MX_I2C1_Init(void)
 }
 
 /**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
   * @brief USART1 Initialization Function
   * @param None
   * @retval None
@@ -278,6 +435,22 @@ static void MX_USART1_UART_Init(void)
 }
 
 /**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA2_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA2_Stream2_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA2_Stream2_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Stream2_IRQn);
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -289,11 +462,14 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LCD_RESET_Pin|LCD_CS_Pin|LCD_A0_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin : LED_Pin */
   GPIO_InitStruct.Pin = LED_Pin;
@@ -302,6 +478,13 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
 
+  /*Configure GPIO pins : LCD_RESET_Pin LCD_CS_Pin LCD_A0_Pin */
+  GPIO_InitStruct.Pin = LCD_RESET_Pin|LCD_CS_Pin|LCD_A0_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   /*Configure GPIO pin : BUTTON_Pin */
   GPIO_InitStruct.Pin = BUTTON_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
@@ -309,51 +492,104 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(BUTTON_GPIO_Port, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+  HAL_NVIC_SetPriority(EXTI4_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(EXTI4_IRQn);
 
 }
 
 /* USER CODE BEGIN 4 */
-/*
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
-	if (htim == &htim2)
-	{
-		HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
-	}
-}
-
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
-	if(GPIO_Pin == BUTTON_Pin)
-	{
-		if (HAL_GPIO_ReadPin(BUTTON_GPIO_Port, BUTTON_Pin) == GPIO_PIN_SET)
-		{
-			__HAL_TIM_SET_COUNTER(&htim3, 0);
-			HAL_TIM_Base_Start(&htim3);
-		}
-		else
-		{
-			HAL_TIM_Base_Stop(&htim3);
-
-			uint32_t cnt = __HAL_TIM_GET_COUNTER(&htim3);
-			if (cnt < 2000)
-			{
-				__HAL_TIM_SET_AUTORELOAD(&htim2, 49999);
-				__HAL_TIM_SET_COUNTER(&htim2, 0);
-			}
-			else
-			{
-				__HAL_TIM_SET_AUTORELOAD(&htim2, 9999);
-				__HAL_TIM_SET_COUNTER(&htim2, 0);
-			}
-		}
-
-	}
 }
-*/
+
 /* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_StartTaskLEDBlink */
+/**
+  * @brief  Function implementing the taskLEDBlink thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartTaskLEDBlink */
+void StartTaskLEDBlink(void *argument)//producer
+{
+  /* USER CODE BEGIN 5 */
+
+	uint32_t pressure = 0;
+	uint32_t temperature = 0;
+
+	//osSemaphoreRelease(sem2Handle);
+	/* Infinite loop */
+	for(;;)
+	{
+		pressure = baro_read_press();
+		temperature = baro_read_temp();
+
+		osSemaphoreAcquire(sem1Handle, osWaitForever);//sync with consumer
+
+		osMutexAcquire(muxUARTHandle, osWaitForever);
+
+		sprintf(text, "*+%ld.%02ld,%ld.%02ld+*\n", temperature/100, temperature%100, pressure/100, pressure%100);
+
+		osMutexRelease(muxUARTHandle);
+
+
+		osSemaphoreRelease(sem2Handle);
+		osDelay(pdMS_TO_TICKS(100));
+
+		//osSemaphoreAcquire(sem2Handle, osWaitForever);
+	}
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_StartTaskButtonRead */
+/**
+* @brief Function implementing the taskButtonRead thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTaskButtonRead */
+void StartTaskButtonRead(void *argument)//consumer
+{
+  /* USER CODE BEGIN StartTaskButtonRead */
+  /* Infinite loop */
+  for(;;)
+  {
+	  osSemaphoreAcquire(sem2Handle, osWaitForever);//sync with producer
+
+	  osMutexAcquire(muxUARTHandle, osWaitForever);
+
+	  HAL_UART_Transmit(&huart1, (uint8_t *)text, strlen(text), HAL_MAX_DELAY);
+
+	  osMutexRelease(muxUARTHandle);
+
+	  osSemaphoreRelease(sem1Handle);
+	  osDelay(pdMS_TO_TICKS(100));
+
+  }
+  /* USER CODE END StartTaskButtonRead */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM1 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM1) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
 
 /**
   * @brief  This function is executed in case of error occurrence.
--- project/Core/Src/stm32f4xx_it.c
+++ project/Core/Src/stm32f4xx_it.c
@@ -55,6 +55,8 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern DMA_HandleTypeDef hdma_usart1_rx;
+extern TIM_HandleTypeDef htim1;
 
 /* USER CODE BEGIN EV */
 
@@ -139,19 +141,6 @@ void UsageFault_Handler(void)
 }
 
 /**
-  * @brief This function handles System service call via SWI instruction.
-  */
-void SVC_Handler(void)
-{
-  /* USER CODE BEGIN SVCall_IRQn 0 */
-
-  /* USER CODE END SVCall_IRQn 0 */
-  /* USER CODE BEGIN SVCall_IRQn 1 */
-
-  /* USER CODE END SVCall_IRQn 1 */
-}
-
-/**
   * @brief This function handles Debug monitor.
   */
 void DebugMon_Handler(void)
@@ -164,33 +153,6 @@ void DebugMon_Handler(void)
   /* USER CODE END DebugMonitor_IRQn 1 */
 }
 
-/**
-  * @brief This function handles Pendable request for system service.
-  */
-void PendSV_Handler(void)
-{
-  /* USER CODE BEGIN PendSV_IRQn 0 */
-
-  /* USER CODE END PendSV_IRQn 0 */
-  /* USER CODE BEGIN PendSV_IRQn 1 */
-
-  /* USER CODE END PendSV_IRQn 1 */
-}
-
-/**
-  * @brief This function handles System tick timer.
-  */
-void SysTick_Handler(void)
-{
-  /* USER CODE BEGIN SysTick_IRQn 0 */
-
-  /* USER CODE END SysTick_IRQn 0 */
-  HAL_IncTick();
-  /* USER CODE BEGIN SysTick_IRQn 1 */
-
-  /* USER CODE END SysTick_IRQn 1 */
-}
-
 /******************************************************************************/
 /* STM32F4xx Peripheral Interrupt Handlers                                    */
 /* Add here the Interrupt Handlers for the used peripherals.                  */
@@ -199,17 +161,45 @@ void SysTick_Handler(void)
 /******************************************************************************/
 
 /**
-  * @brief This function handles EXTI line[15:10] interrupts.
+  * @brief This function handles EXTI line4 interrupt.
   */
-void EXTI15_10_IRQHandler(void)
+void EXTI4_IRQHandler(void)
 {
-  /* USER CODE BEGIN EXTI15_10_IRQn 0 */
+  /* USER CODE BEGIN EXTI4_IRQn 0 */
 
-  /* USER CODE END EXTI15_10_IRQn 0 */
+  /* USER CODE END EXTI4_IRQn 0 */
   HAL_GPIO_EXTI_IRQHandler(BUTTON_Pin);
-  /* USER CODE BEGIN EXTI15_10_IRQn 1 */
+  /* USER CODE BEGIN EXTI4_IRQn 1 */
 
-  /* USER CODE END EXTI15_10_IRQn 1 */
+  /* USER CODE END EXTI4_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM1 update interrupt and TIM10 global interrupt.
+  */
+void TIM1_UP_TIM10_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 0 */
+
+  /* USER CODE END TIM1_UP_TIM10_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim1);
+  /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 1 */
+
+  /* USER CODE END TIM1_UP_TIM10_IRQn 1 */
+}
+
+/**
+  * @brief This function handles DMA2 stream2 global interrupt.
+  */
+void DMA2_Stream2_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA2_Stream2_IRQn 0 */
+
+  /* USER CODE END DMA2_Stream2_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_usart1_rx);
+  /* USER CODE BEGIN DMA2_Stream2_IRQn 1 */
+
+  /* USER CODE END DMA2_Stream2_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
