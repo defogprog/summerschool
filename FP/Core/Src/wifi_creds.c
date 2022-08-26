442f57d636e0954901bf92c9bace668eceadf008
Denys Novohatko <denys.novohatko@gmail.com>
2022-08-26 17:44:37 +0300

    final project

---
+++ FP/Core/Src/wifi_creds.c
@@ -0,0 +1,10 @@
+/*
+ * wifi_creds.c
+ *
+ *  Created on: Aug 24, 2022
+ *      Author: Admin
+ */
+
+
+char wifi_ap[] = "TP-LINK_B6E62E"; //"AJAX | Guest";
+char wifi_pass[] = "68853778"; //"Veridian8";
